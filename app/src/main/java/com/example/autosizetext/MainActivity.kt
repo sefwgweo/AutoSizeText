991cae8d173d430721fb542987447b6e6790af6f
Masato Haruta <masato.haruta@uzabase.com>
2023-06-26 20:18:45 +0900

    initial commit

---
+++ app/src/main/java/com/example/autosizetext/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.autosizetext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
